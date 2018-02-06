7b93812590f84dbbf6b9ae00a0dab1cd906a5735
e155745 <e155745@ie.u-ryukyu.ac.jp>
2018-02-06 22:07:26 +0900

    JSでトップページに動きをつけた。

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,49 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+// $(window).on('load',function(){
+//
+//     $(window).scroll(function (){
+//         $('.fade-in').each(function(){
+//             var POS = $(this).offset().top;
+//             var scroll = $(window).scrollTop();
+//             var windowHeight = $(window).height(); 
+//
+//             if (scroll > POS - windowHeight + windowHeight/3){
+//                 $(this).css("opacity","1" );
+//             } else {
+//                 $(this).css("opacity","0" );
+//             }
+//         });
+//     });
+// });
+
+$(window).on('load',function(){
+
+
+    $(window).scroll(function (){
+        $('.fade-up').each(function(){
+            var POS = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+
+            if (scroll > POS - windowHeight){
+                $(this).css({
+                        'opacity':'1',
+                        'transform':'translateY(0)',
+                        '-webkit-transform':'translateY(0)',
+                        '-moz-transform':'translateY(0)',
+                        '-ms-transform':'translateY(0)'
+                });
+            } else {
+                $(this).css({
+                        'opacity':'0',
+                        'transform':'translateY(70px)',
+                        '-webkit-transform':'translateY(70px)',
+                        '-moz-transform':'translateY(70px)',
+                        '-ms-transform':'translateY(70px)'
+                });
+            }
+        });
+    });
+});
