27b95045272d9d05e5ae086aafb77f3537707ae7
e155745 <e155745@ie.u-ryukyu.ac.jp>
2018-02-12 17:20:39 +0900

    routingの書き方修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root 'home#top'
-  get 'posts/index'
-  get "/posts/new" => "posts#new"
-  post "posts/create" => "posts#create"
-  get "posts/index" => "posts#index"
+  get '/posts/index', to: 'posts#index'
+  get '/posts/new', to: 'posts#new'
+  post '/posts/create', to: 'posts#create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
