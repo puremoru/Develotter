3b368dd501f495e85097a5c120ad6b0d8de04713
e155745 <e155745@ie.u-ryukyu.ac.jp>
2018-02-16 21:17:33 +0900

    投稿詳細ページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'home#top'
-  get '/posts/index', to: 'posts#index'
   get '/posts/new', to: 'posts#new'
   post '/posts/create', to: 'posts#create'
+  get '/posts/index', to: 'posts#index'
+  get '/posts/:id', to: 'posts#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
