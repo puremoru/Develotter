2fef35a022630e9580ea42bb54bd02528656af89
e155745 <e155745@ie.u-ryukyu.ac.jp>
2018-02-20 01:32:44 +0900

    :providerに戻す

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '/auth/twitter/callback', to: 'sessions#create'
+  get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
   root 'home#top'
