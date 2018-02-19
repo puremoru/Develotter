1e9ec1bded9fd6b46a2a365d53334c3219245200
e155745 <e155745@ie.u-ryukyu.ac.jp>
2018-02-19 23:53:25 +0900

    @userをuserに修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_from_auth(request.env['omniauth.auth'])
-    session[:user_id] = @user.id
+    user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+    session[:user_id] = user.id
     redirect_to root_path
   end
 
