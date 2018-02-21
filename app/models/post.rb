46fe8266b156ffa1da308c9fa79a738482ad0d1e
e155745 <e155745@ie.u-ryukyu.ac.jp>
2018-02-21 18:38:49 +0900

    validationを外してみた

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :name, {presence: true}
   validates :about, {presence: true, length: {maximum: 200}}
-  validates :user_id, {presence: true}
+  # validates :user_id, {presence: true}
 
   def user
     return User.find_by(id: self.user_id)
