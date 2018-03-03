5db74a830e18464fe5e97ebaf1c235614c8c717c
e155745 <e155745@ie.u-ryukyu.ac.jp>
2018-03-04 01:37:46 +0900

    新規投稿のサービスの説明のフォームの文字入力数を可視化

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -75,3 +75,20 @@ $(window).on('load',function(){
         });
     });
 });
+
+// 文字数のカウント
+$(function () {
+  $("#about-textarea").keyup(function(){
+    var counter = $(this).val().length;
+    $("#countUp").text(counter);
+
+    if(counter == 0){
+      $("#countUp").text("0");
+    }
+    if(counter >= 200){
+      $("#countUp").css("color","red");
+    } else {
+      $("#countUp").css("color","#666");
+    }
+  });
+});
