2eff579882c5afb4d07842f8752789b2b2c07eaa
e155745 <e155745@ie.u-ryukyu.ac.jp>
2018-03-04 14:24:51 +0900

    入力文字数の表示のJSが動かないので一旦外す

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -74,21 +74,21 @@ $(window).on('load',function(){
             }
         });
     });
-    $(function () {
-      $("#about-textarea").keyup(function(){
-        var counter = $(this).val().length;
-        $("#countUp").text(counter);
-
-        if(counter == 0){
-          $("#countUp").text("0");
-        }
-        if(counter >= 200){
-          $("#countUp").css("color","red");
-        } else {
-          $("#countUp").css("color","#666");
-        }
-      });
-    });
+    // $(function () {
+    //   $("#about-textarea").keyup(function(){
+    //     var counter = $(this).val().length;
+    //     $("#countUp").text(counter);
+    //
+    //     if(counter == 0){
+    //       $("#countUp").text("0");
+    //     }
+    //     if(counter >= 200){
+    //       $("#countUp").css("color","red");
+    //     } else {
+    //       $("#countUp").css("color","#666");
+    //     }
+    //   });
+    // });
   //   $(function(){
   //     var countMax = 200;
   //     $('#about-textarea').bind('keydown keyup keypress change',function(){
