fc427fda6879ecf6676ee9c9fa4983a1b44f99fd
e155745 <e155745@ie.u-ryukyu.ac.jp>
2018-03-09 12:15:36 +0900

    heroku上で画像が表示されなくなるエラーの解決

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   GA.tracker = "UA-115001259-1"
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
